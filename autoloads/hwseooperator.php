60009f9f50f3dcfb5cb2cef72e9e565d9a66fe1f
Mathias Geat <mathias.geat@holzweg.com>
2011-08-31 15:01:26 +0200

    add class level docblock

--- autoloads/hwseooperator.php
+++ autoloads/hwseooperator.php
@@ -1,4 +1,41 @@
 <?php
+/**
+* HwSEO
+*
+* Copyright (c) 2011 holzweg e-commerce solutions - http://www.holzweg.com
+* All rights reserved.
+*
+* NOTICE OF LICENSE
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* @category HwSEO
+* @package HwSEO
+* @author Mathias Geat <mathias.geat@holzweg.com>
+* @copyright 2011 holzweg e-commerce solutions - http://www.holzweg.com
+* @license http://www.gnu.org/licenses/gpl.txt GNU GPL v3
+*/
+
+/**
+* HwSEO Operator
+*
+* @category HwSEO
+* @package HwSEO
+* @author Mathias Geat <mathias.geat@holzweg.com>
+* @copyright 2011 holzweg e-commerce solutions - http://www.holzweg.com
+* @license http://www.gnu.org/licenses/gpl.txt GNU GPL v3
+*/
 class HwSEOOperator
 {
     function HwSEOOperator()
